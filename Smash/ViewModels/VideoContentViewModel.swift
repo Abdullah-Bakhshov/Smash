41bf8f31320feb623bbf067934b2e493540dba40
Abdullah-Bakhshov <140121021+Abdullah-Bakhshov@users.noreply.github.com>
2025-01-02 08:09:03 +0000

    fixed audio issue

--- Smash/ViewModels/VideoContentViewModel.swift
+++ Smash/ViewModels/VideoContentViewModel.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import Aespa
 import Observation
+import AVFAudio
 
 @Observable
 final class VideoContentViewModel {
@@ -29,12 +30,28 @@ final class VideoContentViewModel {
     }
     
     func SetUp() {
+        configureAudioSession()
         aespaSession
             .common(.focus(mode: .continuousAutoFocus))
             .common(.changeMonitoring(enabled: true))
             .common(.orientation(orientation: .portrait))
             .common(.quality(preset: .high))
         aespaSession.video(.unmute)
+    }
+    
+    private func configureAudioSession() {
+        do {
+            let audioSession = AVAudioSession.sharedInstance()
+            try audioSession.setCategory(.playAndRecord, options: [.defaultToSpeaker, .allowBluetooth])
+            try audioSession.setMode(.videoRecording) // change between voice chat or video recording
+            try audioSession.setActive(true)
+            
+            if audioSession.mode == .voiceChat {
+                print("AGC control is active. Ensure no unnecessary processing happens.")
+            }
+        } catch {
+            print("Failed to configure AVAudioSession: \(error)")
+        }
     }
     
     @objc func StartandStopRecording() {
